898c17189851f376900bdc33d152d391791b8d16
Irtaza <irtaza010@gmail.com>
2025-01-19 15:54:12 +0500

    fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,8 @@ import { getRequestConfig } from 'next-intl/server';
 
 const locales = ['en', 'ar'];
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
   if (!locale || !locales.includes(locale)) {
     console.error('Invalid locale:', locale);
     notFound();
